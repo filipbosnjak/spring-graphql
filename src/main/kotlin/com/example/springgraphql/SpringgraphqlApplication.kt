3eab087889eafc1789ab1721b15da8309ee4f1be
filipbosnjak <filipo.bosnjak@gmail.com>
2023-06-09 20:54:07 +0200

    initial commit

---
+++ src/main/kotlin/com/example/springgraphql/SpringgraphqlApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springgraphql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringgraphqlApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringgraphqlApplication>(*args)
+}
